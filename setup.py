b6e08941840dafa3c159ade01fede973af55fd26
藤森恭兵 <s2022029@stu.musashino-u.ac.jp>
2022-06-08 04:00:02 +0900

    修正

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="kotonohagetter", # 設定する名前
+    name="timeofsleep", # 設定する名前
     version="0.0.1", # バージョン設定
-    author="kyohei fujimori", # 名前
-    author_email="ohnishi.rikito@gmail.com", # メアド変更
-    description='A package for visualization of aggregate data of players in "Kotoha Tango"', # 説明文書書き換え
+    author="kyohei fujimori.kyohei@gmail.com", # 名前
+    author_email="", # メアド変更
+    description='It is a package that visualizes sleep time and sleep quality.', # 説明文書書き換え
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/plumchloride/kotonohatango-getter", # GitHubURL
+    url="https://github.com/kyohei-2022029/timeofsleep/tree/master", # GitHubURL
     project_urls={
-        "Bug Tracker": "https://github.com/plumchloride/kotonohatango-getter", #GitHubURL
+        "Bug Tracker": "https://github.com/kyohei-2022029/timeofsleep/tree/master", #GitHubURL
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -21,12 +21,12 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    py_modules=['kotonohagetter'], # 設定するモジュール名
+    py_modules=['timeofsleep'], # 設定するモジュール名
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.8",
     entry_points = {
         'console_scripts': [
-            'kotonohagetter = kotonohagetter:main' # srcの中にある.pyの手前の文字
+            'timeofsleep = timeofsleep:main' # srcの中にある.pyの手前の文字
         ]
     },
 )
--- sleep.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import sys,os
- 
-def main():
-    df = pd.read_csv("data/sleep_data.csv")
-    print(df)
-    
-main()
